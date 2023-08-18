6df7355962bd13979935f364dbb272ff4bd212eb
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2023-08-18 12:38:23 +0530

    useState commit

--- src/Sponsor_details/Sdetails.jsx
+++ src/Sponsor_details/Sdetails.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sdetails.css'
+import axios from './axios'
 import Sponsor_details from '../Images/Sponsor_details.png'
 const Sdetails = () => {
+  const [Sname,setSname]=useState("")
+  const [Semail,setSemail]=useState("")
+  const [Smob,setSmob]=useState("")
   return (
     <div>
     <div className='Sdetails'>
       <div className="Sponsor_image">
-        <img src={Sponsor_details} alt="" />
+        <img src={Sponsor_details} alt=""/>
       </div>
       <div className="Sponsor_form">
         <h1>YOUR DETAILS</h1>
         <form action="">
             <label htmlFor="">Name</label>
             <br />
-            <input type="text" />
+            <input type="text" name="Sname" onChange={(e)=>{
+          setSname(e.target.value)
+          }} />
             <br />
             <label htmlFor="">Email</label>
             <br />
-            <input type="email" />
+            <input type="email" name="Semail" onChange={(e)=>{
+          setSemail(e.target.value)
+          }} />
             <br />
             <label htmlFor="">Mobile Number</label>
             <br />
-            <input type="text" />
+            <input type="text" name="Smob" onChange={(e)=>{
+          setSmob(e.target.value)
+          }}/>
         </form>
         <button>Submit</button>
       </div>
