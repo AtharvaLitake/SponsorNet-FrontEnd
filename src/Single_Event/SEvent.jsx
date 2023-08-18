ee044e07aa76f557b76b023ef85f23ab41974b42
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2023-08-19 04:30:09 +0530

    Backend Frontend + Functionalities

--- src/Single_Event/SEvent.jsx
+++ src/Single_Event/SEvent.jsx
@@ -2,39 +2,49 @@ import React from 'react'
 import './SEvent.css'
 import NavbarC from '../Navbar/NavbarC'
 import EventImage from '../Images/EventImage.png'
-import {useLocation,Link} from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
+import { useCookies } from "react-cookie";
+import Login from '../Components/Login'
 const SEvent = () => {
-  const location=useLocation()
-  const event=location.state
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const location = useLocation()
+  const event = location.state
   console.log(event)
   return (
     <>
-    <NavbarC></NavbarC>
-    <div className='Single_Event_page'>
-      <div className="Event_image">
-        <img src={EventImage} alt="" />
-      </div>
-      <div className="Event_detail">
-        <div className="event_information">
-        <h1>{event.val.event_name.toUpperCase()}</h1>
-        <br />
-        <h3>Date : {event.val.event_date}</h3>
-        <br />
-        <h3>Type : {event.val.event_type}</h3>
-        <br />
-        <h3>FootFall : {event.val.event_footfall}</h3>
-        <br />
-        <h3>Deliverables : {event.val.deliverables}</h3>
-        <br />
-        <h2>Amount Expected : {event.val.sponsorship_amount}</h2>
-        <br />
-        <Link to='/sponsorDetails' state={{event}}>
-        <button> APPLY </button>
-        </Link>
-        </div>
+      {
+        cookies.AuthToken ? (
+          <div>
+            <NavbarC></NavbarC>
+            <div className='Single_Event_page'>
+              <div className="Event_image">
+                <img src={EventImage} alt="" />
+              </div>
+              <div className="Event_detail">
+                <div className="event_information">
+                  <h1>{event.val.event_name.toUpperCase()}</h1>
+                  <br />
+                  <h3>Date : {event.val.event_date}</h3>
+                  <br />
+                  <h3>Type : {event.val.event_type}</h3>
+                  <br />
+                  <h3>FootFall : {event.val.event_footfall}</h3>
+                  <br />
+                  <h3>Deliverables : {event.val.deliverables}</h3>
+                  <br />
+                  <h2>Amount Expected : {event.val.sponsorship_amount}</h2>
+                  <br />
+                  <Link to='/sponsorDetails' state={{ event }}>
+                    <button> APPLY </button>
+                  </Link>
+                </div>
 
-      </div>
-    </div>
+              </div>
+            </div>
+          </div>
+        ) : (<Login></Login>)
+      }
+
     </>
   )
 }
