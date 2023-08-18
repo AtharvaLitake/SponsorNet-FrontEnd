e7ecd82be8261d838eeff8c398fd818b36e73b0b
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2023-08-19 00:56:48 +0530

    integration-event,sponsor

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import './Login.css'
 import login_page from '../Images/Login_Image.png'
+import {useState} from 'react'
+import axios from '../axios'
 const Login = () => {
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const LoginUser=async (e)=>{
+    e.preventDefault()
+   const response=await axios.post("/login",{
+      user_email:email,
+      user_password:password
+    })
+    if(response.data.detail)
+    {
+      alert("Error! Incorrect Email-Id or Password ")
+    }
+    else{
+      alert("Logged in Successfully")
+    }
+    
+}
   return (
     <div className='login_Page'>
       <div className="login_image">
@@ -12,13 +31,17 @@ const Login = () => {
         <form action="">
             <label htmlFor="">Email-Id</label>
             <br />
-            <input type="email" />
+            <input type="email" name="email" onChange={(e)=>{
+              setEmail(e.target.value)
+            }}/>
             <br />
             <label htmlFor="">Password</label>
             <br />
-            <input type="password" />
+            <input type="password" name="password" onChange={(e)=>{
+              setPassword(e.target.value)
+            }}/>
         </form>
-        <button>Submit</button>
+        <button onClick={LoginUser}>Submit</button>
         <h3>Not a Member Yet ?</h3>
         <h3>Sign Up Now</h3>
       </div>
